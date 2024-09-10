de23e33a5bb346dd1c40215f534fb8f37b390c72
bil <bilroum@gmail.com>
2024-09-10 16:42:10 +0200

    Initial commit with website files

---
+++ dist/js/index.js
@@ -0,0 +1,61 @@
+//Spinner
+window.addEventListener("load", () => {
+  // Simulate loading delay
+  setTimeout(() => {
+    // Hide spinner and show content
+    document.getElementById("spinner").style.display = "none";
+    document.querySelectorAll("content").classList.remove("hidden"); //class content hidden and not
+  }, 2000); // 2000ms = 2 seconds
+});
+
+// JavaScript to toggle the open class
+document.querySelector(".burger-icon").addEventListener("click", function () {
+  this.classList.toggle("open");
+  document.querySelector("#mobile-menu").classList.toggle("open");
+});
+
+//Hero section with arows and indicators
+document.addEventListener("DOMContentLoaded", () => {
+  const carousel = document.querySelector(".carousel");
+  const items = document.querySelectorAll(".carousel-item");
+  const prevButton = document.querySelector(".carousel-prev");
+  const nextButton = document.querySelector(".carousel-next");
+  const indicators = document.querySelectorAll(".carousel-indicator");
+
+  let currentIndex = 0;
+
+  function showCarouselItem(index) {
+    const offset = -index * 100;
+    carousel.style.transform = `translateX(${offset}%)`;
+    updateIndicators();
+  }
+
+  function nextItem() {
+    currentIndex = (currentIndex + 1) % items.length;
+    showCarouselItem(currentIndex);
+  }
+
+  function prevItem() {
+    currentIndex = (currentIndex - 1 + items.length) % items.length;
+    showCarouselItem(currentIndex);
+  }
+
+  function updateIndicators() {
+    indicators.forEach((indicator, index) => {
+      indicator.classList.toggle("active", index === currentIndex);
+    });
+  }
+
+  indicators.forEach((indicator, index) => {
+    indicator.addEventListener("click", () => {
+      currentIndex = index;
+      showCarouselItem(currentIndex);
+    });
+  });
+
+  // Initialize
+  showCarouselItem(currentIndex);
+
+  nextButton.addEventListener("click", nextItem);
+  prevButton.addEventListener("click", prevItem);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      backgroundImage: {
+        "bg-front": "url('src/img/DSCF0186.jpg')",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat"],
+      },
+    },
+  },
+  plugins: [],
+};
